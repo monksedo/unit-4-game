1ce8a5d146b5c3290b55bfb007589fa8552d7e38
Monk Sedo <fullstackdev1828@outlook.com>
2019-10-20 03:16:32 -0700

    Add project files to repot

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 # unit-4-game
-An interactive fun game for web browsers, the app is dynamically update the HTML page with JQuery library
 
+## Crystal Collection Game
+The goal for the assignment is the create an interactive fun game for web browsers,the app is dynamically update the HTML page with JQuery DOM manipulation library, the will be coding in HTML, CSS, Javascript and JQuery.
+
+## Technology Used
+HTML, CSS, JavaScript, & JQuery
+
+## Game rules
+<h2>How to play?</h2>
+<p>1. The game will start with a random score for you to match.</p>
+<p>2. Click on the crystals bellow to collect points for your total score.</p>
+<p>3. You win the game when you total score matchs the random score.</p>
+<p>4. You loss the game when your total score is higher than the randome score.</p>
+<p>5. The game will let you know when you win or loss the round.</p>
+
+## Homework Assignment Instruction
 [jQuery Assignment - Intruction](https://ucb.bootcampcontent.com/UCB-Coding-Bootcamp/UCB-VIRT-FSF-PT-09-2019-U-O/blob/master/course-content/04-jquery/homework/Instructions/homework_instructions.md)
+
+## Start the Game
+[Click here to start](https://monksedo.github.io/unit-4-game/)
+
+## CSS Code Snippets
+![Code Block 1](images/css01.png)
+
+## JavaScrip Code 01
+![Code Block 01](images/js01.png)
+
+## JavaScrip Code 02
+![Code Block 01](images/js02.png)
+
+## JavaScrip Code 03
+![Code Block 01](images/js03.png)
+
+## App Screen 01
+![App Screen 01](images/game01.png)
+
+## App Screen 02
+![App Screen 01](images/game02.png)
+
 
---
+++ assets/javascript/crystalGame.js
@@ -0,0 +1,101 @@
+////////////////////////////////////////////////////////////////////////
+// Beginning code   
+  
+  $( document ).ready(function(){
+  // Starting the game with a random score
+  var randomStart=Math.floor(Math.random()*102+19)
+  
+  // Print randome start number to HTML id=randomScore
+  $('#randomScore').text(randomStart);
+  
+  // Start and random score for each crystal
+  var purple, green, blue, red;
+  purple = Math.floor(Math.random()*12+1)
+  green = Math.floor(Math.random()*12+1)
+  blue = Math.floor(Math.random()*12+1)
+  red = Math.floor(Math.random()*12+1)
+  
+  // Game score varialbes
+  var playerTotal= 0; 
+  var wins= 0;
+  var losses = 0;
+  
+
+$('#wins').text(wins);
+$('#losses').text(losses);
+
+// Restart new game
+function resetGame(){
+      randomStart=Math.floor(Math.random()*102+19);
+      console.log(randomStart)
+      $('#randomScore').text(randomStart);
+      purple = Math.floor(Math.random()*12+1);
+      green = Math.floor(Math.random()*12+1);
+      blue = Math.floor(Math.random()*12+1);
+      red = Math.floor(Math.random()*12+1);
+      playerTotal= 0;
+      $('#totalScore').text(playerTotal);
+      } 
+// Display winnnig Game
+function winningGame() {
+  alert("Congrats! You won!");
+  wins++; 
+  $('#wins').text(wins);
+  resetGame();
+}
+// Display losses
+function lossingGame(){
+alert ("Game Over!");
+  losses++;
+  $('#losses').text(losses);
+  resetGame()
+}
+// Clicking crystals
+  $('.purple').on ('click', function(){
+    playerTotal = playerTotal + purple;
+    console.log("New playerTotal= " + playerTotal);
+    $('#totalScore').text(playerTotal); 
+          //Win & lose conditions
+        if (playerTotal == random){
+          winningGame();
+        }
+        else if ( playerTotal > random){
+          lossingGame();
+        }   
+  })  
+  $('.green').on ('click', function(){
+    playerTotal = playerTotal + green;
+    console.log("New playerTotal= " + playerTotal);
+    $('#totalScore').text(playerTotal); 
+        if (playerTotal == random){
+          winningGame();
+        }
+        else if ( playerTotal > random){
+          lossingGame();
+        } 
+  })  
+  $('.blue').on ('click', function(){
+    playerTotal = playerTotal + blue;
+    console.log("New playerTotal= " + playerTotal);
+    $('#totalScore').text(playerTotal);
+
+          if (playerTotal == randomStart){
+          winningGame();
+        }
+        else if ( playerTotal > randomStart){
+          lossingGame();
+        } 
+  })  
+  $('.red').on ('click', function(){
+    playerTotal = playerTotal + red;
+    console.log("New playerTotal= " + playerTotal);
+    $('#totalScore').text(playerTotal); 
+      
+          if (playerTotal == randomStart){
+          winningGame();
+        }
+        else if ( playerTotal > randomStart){
+          lossingGame();
+        }
+  });   
+}); 
